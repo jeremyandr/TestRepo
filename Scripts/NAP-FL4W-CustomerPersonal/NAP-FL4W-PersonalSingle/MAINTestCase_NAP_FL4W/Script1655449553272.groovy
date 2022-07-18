e7a7bd98350f0429701ae05d5766e3058486fec3
Jeremy Andreas (Adins) <jeremy.andreas@ad-ins.com>
2022-07-18 17:01:59 +0700

    tre

--- Scripts/NAP-FL4W-CustomerPersonal/NAP-FL4W-PersonalSingle/MAINTestCase_NAP_FL4W/Script1655449553272.groovy
+++ Scripts/NAP-FL4W-CustomerPersonal/NAP-FL4W-PersonalSingle/MAINTestCase_NAP_FL4W/Script1655449553272.groovy
@@ -24,6 +24,7 @@ String filePath = userDir + GlobalVariable.Path
 GlobalVariable.DataFilePath = filePath
 
 println(GlobalVariable.DataFilePath)
+//
 
 if (GlobalVariable.Role == 'Data Entry') {
     for (GlobalVariable.NumofColm; GlobalVariable.NumofColm <= (Integer.parseInt(GlobalVariable.CountNumofCust) + 1); (GlobalVariable.NumofColm)++) {
